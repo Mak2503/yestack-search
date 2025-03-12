8cdf070f012cff18c18dbe8f4630b8f1a564b8bc
Mohamed Abdul Kader <mak2503sde@gmail.com>
2025-03-12 13:56:19 +0530

    pass search state and handle from App to search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { useCallback, useState } from "react";
 import "./App.css";
 import SearchInput from "./components/SearchInput";
 
 function App() {
+  // const [count, setCount] = useState(0)
+  const [search, setSearch] = useState("");
+  const handleSearch = useCallback((value: string) => {
+    setSearch(value);
+  }, []);
+
   return (
     <main>
-      <SearchInput />
+      <SearchInput search={search} onChange={handleSearch} />
+      {/* To test re rendering of search (To be removed) */}
+      {/* <div>
+        <button onClick={() => setCount(count + 1)}>+</button>
+        {count}
+      </div> */}
     </main>
   );
 }
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,8 +1,29 @@
-const SearchInput = () => {
-  console.log("Mounted")
-  return (
-    <input className="search-input" type="text" autoFocus />
-  )
+import { ChangeEvent, memo } from "react";
+
+interface SearchInputProps {
+  search: string;
+  onChange: (value: string) => void
 }
 
-export default SearchInput
+const SearchInput: React.FC<SearchInputProps> = ({ search, onChange }) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value)
+  }
+
+  // To test re-rendering (To be removed)
+  console.log("Rendered")
+
+  return (
+    <>
+      <input
+        className="search-input"
+        type="text"
+        value={search}
+        onChange={handleChange}
+      />
+      <pre>{search}</pre>
+    </>
+  );
+};
+
+export default memo(SearchInput);
