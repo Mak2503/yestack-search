b608fc414c8e7dc559a3af55a1ccbfc837fab147
Mohamed Abdul Kader <mak2503sde@gmail.com>
2025-03-13 12:37:19 +0530

    add infinite scrolling when searching products

--- src/components/AdvancedSearch.tsx
+++ src/components/AdvancedSearch.tsx
@@ -3,7 +3,6 @@ import SearchInput from "./SearchInput";
 import SuggestionDropdown from "./SuggestionDropdown";
 
 const Search = () => {
-  // const [count, setCount] = useState(0)
   const [search, setSearch] = useState("");
   const handleSearch = useCallback((value: string) => {
     setSearch(value);
@@ -12,12 +11,7 @@ const Search = () => {
   return (
     <div className="container">
       <SearchInput search={search} onChange={handleSearch} />
-      {/* To test re rendering of search (To be removed) */}
-      {/* <div>
-        <button onClick={() => setCount(count + 1)}>+</button>
-        {count}
-      </div> */}
-      <SuggestionDropdown search={search.toLowerCase().trim()} />
+      <SuggestionDropdown query={search.toLowerCase().trim()} />
     </div>
   );
 };
--- src/components/SuggestionDropdown.tsx
+++ src/components/SuggestionDropdown.tsx
@@ -1,45 +1,98 @@
-import { useQuery } from "@tanstack/react-query";
-import { memo } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { Fragment, memo, useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 import { Product } from "../types";
 import { useDebounce } from "../hooks/useDebounce";
 
-interface SuggestionBoxProps {
-  search: string
+interface SuggestionDropdownProps {
+  query: string;
 }
 
-const SuggestionBox: React.FC<SuggestionBoxProps> = ({ search }) => {
-  const debouncedQuery = useDebounce(search, 300);
-  
-  const fetchProducts = async ({ queryKey }: { queryKey: string[] }) => {
+const SuggestionDropdown: React.FC<SuggestionDropdownProps> = ({ query }) => {
+  const { ref, inView } = useInView();
+  const [page, setPage] = useState(1);
+  const debouncedQuery = useDebounce(query, 300);
+
+  const fetchProducts = async ({
+    queryKey,
+    pageParam,
+  }: {
+    queryKey: string[];
+    pageParam: number | false;
+  }) => {
     const [, query] = queryKey;
+    setPage(pageParam ? pageParam : 1);
     const response = await fetch(
-      `https://fakestoreapi.in/api/products`
+      `https://fakestoreapi.in/api/products?page=${pageParam}&limit=15`
     );
     const data = await response.json();
     const filteredData = data.products.filter((product: Product) => {
       return product.title.toLowerCase().includes(query);
     });
+
     return filteredData;
   };
 
-  const { data, isPending, error } = useQuery({
-    queryKey: ["productsData", debouncedQuery],
-    queryFn: fetchProducts,
-  });
+  const { status, data, error, isFetchingNextPage, fetchNextPage } =
+    useInfiniteQuery({
+      queryKey: ["productsData", debouncedQuery],
+      queryFn: fetchProducts,
+      initialPageParam: 1,
+      getPreviousPageParam: () => page > 1 && page - 1,
+      getNextPageParam: () => page < 10 && page + 1,
+    });
 
-  if (isPending) return "Loading...";
-  if (error) return `An error has occured: ${error.message}`;
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView]);
+
+  if ((status as "idle" | "pending" | "success" | "error") === "pending")
+    return (
+      <div className="suggestion-container">
+        <p style={{ margin: "15px" }}>Loading...</p>
+      </div>
+    );
+  if (status === "error" || error) {
+    return (
+      <div className="suggestion-container">
+        <p style={{ margin: "15px" }}>An error has occured: ${error.message}</p>
+      </div>
+    );
+  }
+  if (!data || data.pages.length === 0) {
+    return (
+      <div className="suggestion-container">
+        <p style={{ margin: "15px" }}>No results found</p>
+      </div>
+    );
+  }
 
   return (
     <div className="suggestion-container">
       <h5 style={{ marginLeft: "15px" }}>Popular Searches</h5>
       <ul>
-        {data.map((product: Product) => (
-          <li key={product.id}>{product.title.slice(0, 40)}...</li>
-        ))}
+        {data.pages.length > 0
+          ? data.pages.map((page) => (
+              <Fragment key={page.nextId}>
+                {page.map((product: Product) => (
+                  <li key={product.id}>{product.title?.slice(0, 70)}...</li>
+                ))}
+              </Fragment>
+            ))
+          : null}
+        {query && (
+          <li
+            ref={ref}
+            style={{ fontSize: 12, fontWeight: "bold", fontStyle: "italic" }}
+          >
+            {isFetchingNextPage ? "Loading more..." : "Nothing more to load"}
+          </li>
+        )}
       </ul>
     </div>
   );
 };
 
-export default memo(SuggestionBox);
+export default memo(SuggestionDropdown);
